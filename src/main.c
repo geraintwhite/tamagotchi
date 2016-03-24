a81f0ac460fa65d5f1008f991969ab7e4d9e1eaa
Geraint White <geraintwhite@gmail.com>
2016-03-24 12:43:40 +0000

    Reorder

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <pebble.h>
 
+
 static Window *s_main_window;
 static GBitmap *s_bitmap;
 static BitmapLayer *s_bitmap_layer;
@@ -8,19 +9,10 @@ static TextLayer *s_time_layer;
 static uint32_t creature_states[2] = {RESOURCE_ID_001_SPRAT_IDLE1, RESOURCE_ID_001_SPRAT_IDLE2};
 static int current_state = 0;
 
-static void update_time() {
-  time_t temp = time(NULL);
-  struct tm *tick_time = localtime(&temp);
-  static char s_buffer[8];
-  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
-  text_layer_set_text(s_time_layer, s_buffer);
-}
 
-static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  update_time();
-}
-
-static void change_creature_state() {
+static void
+change_creature_state()
+{
   current_state = (current_state + 1) % (sizeof(creature_states) / sizeof(uint32_t));
   gbitmap_destroy(s_bitmap);
   s_bitmap = gbitmap_create_with_resource(creature_states[current_state]);
@@ -29,35 +21,49 @@ static void change_creature_state() {
   app_timer_register(500, change_creature_state, NULL);
 }
 
-static void main_window_load(Window *window) {
+static void
+update_time()
+{
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
+  text_layer_set_text(s_time_layer, s_buffer);
+}
+
+static void
+tick_handler(struct tm *tick_time, TimeUnits units_changed)
+{
+  update_time();
+}
+
+static void
+main_window_load(Window *window)
+{
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
   s_time_layer = text_layer_create(GRect(0, 8, bounds.size.w, 50));
-
-  text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_text(s_time_layer, "00:00");
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_34_MEDIUM_NUMBERS));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 
   s_bitmap_layer = bitmap_layer_create(bounds);
-
-  change_creature_state();
-
   bitmap_layer_set_compositing_mode(s_bitmap_layer, GCompOpSet);
   layer_add_child(window_layer, bitmap_layer_get_layer(s_bitmap_layer));
 }
 
-static void main_window_unload(Window *window) {
+static void
+main_window_unload(Window *window)
+{
   bitmap_layer_destroy(s_bitmap_layer);
   gbitmap_destroy(s_bitmap);
   text_layer_destroy(s_time_layer);
 }
 
-static void init() {
+static void
+init()
+{
   s_main_window = window_create();
   window_set_background_color(s_main_window, GColorWhite);
   window_set_window_handlers(s_main_window, (WindowHandlers) {
@@ -67,14 +73,19 @@ static void init() {
   window_stack_push(s_main_window, true);
 
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  change_creature_state();
   update_time();
 }
 
-static void deinit() {
+static void
+deinit()
+{
   window_destroy(s_main_window);
 }
 
-int main() {
+int
+main()
+{
   init();
   app_event_loop();
   deinit();
