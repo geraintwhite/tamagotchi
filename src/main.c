7f8b0212cd6cab321e9b8153a173eb7b1528944a
Geraint White <geraintwhite@gmail.com>
2016-03-24 11:02:33 +0000

    Add a watchface

--- src/main.c
+++ src/main.c
@@ -3,9 +3,22 @@
 static Window *s_main_window;
 static GBitmap *s_bitmap;
 static BitmapLayer *s_bitmap_layer;
+static TextLayer *s_time_layer;
 
 static uint32_t creature_states[2] = {RESOURCE_ID_001_SPRAT_IDLE1, RESOURCE_ID_001_SPRAT_IDLE2};
 static int current_state = 0;
+
+static void update_time() {
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
+  text_layer_set_text(s_time_layer, s_buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
 
 static void change_creature_state() {
   current_state = (current_state + 1) % (sizeof(creature_states) / sizeof(uint32_t));
@@ -20,6 +33,16 @@ static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
+  s_time_layer = text_layer_create(GRect(0, 8, bounds.size.w, 50));
+
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text(s_time_layer, "00:00");
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_34_MEDIUM_NUMBERS));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+
   s_bitmap_layer = bitmap_layer_create(bounds);
 
   change_creature_state();
@@ -31,6 +54,7 @@ static void main_window_load(Window *window) {
 static void main_window_unload(Window *window) {
   bitmap_layer_destroy(s_bitmap_layer);
   gbitmap_destroy(s_bitmap);
+  text_layer_destroy(s_time_layer);
 }
 
 static void init() {
@@ -41,6 +65,9 @@ static void init() {
     .unload = main_window_unload,
   });
   window_stack_push(s_main_window, true);
+
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  update_time();
 }
 
 static void deinit() {
