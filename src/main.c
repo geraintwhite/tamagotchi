448459fe16ecb38916de6b3da64b293949633e1c
Sullivan Ford <sullyjhf@hotmail.co.uk>
2016-03-29 22:28:58 +0100

    Rewrote main.c to just have one app_timer

--- src/main.c
+++ src/main.c
@@ -1,44 +1,42 @@
 #include <pebble.h>
+#include <limits.h>
 
 static Window *s_main_window;
 static GBitmap *s_bitmap;
+static Layer *s_canvas_layer;
 static BitmapLayer *s_bitmap_layer;
 static TextLayer *s_time_layer;
+static AppTimer *s_timer;
 
-static uint32_t creature_states[2] = { RESOURCE_ID_001_SPRAT_IDLE1, RESOURCE_ID_001_SPRAT_IDLE2 };
-static uint32_t animation_sequence[10];
-static int current_state = 0;
-static int animation_frame = 0;
-static int total_frames = 0;
+static uint32_t delta = 100; // 10UPS
+static int updates = 0;
+
+static int current_idle_frame = 0;
+static int hatch_animation_frame = 0;
+
 
 // make start time install time or first run time
 // then store it in memory and get from there instead of setting to 0 every time
 static int start_time = 0L;
 static int last_time = 0L;
 
-static bool hatched = 0;
-
-//#define time_t START_TIME = time(NULL);
-
 struct Monster {
   int id;
   char name[16];
-  // 0: egg
-  // 1: idle
-  // 2: sleeping
-  // 3: sick
-  // 4: hungry
-  // 5: dirty
-  // 6: bored
-  // 7: dead
+  // 0: egg, 1: hatching, 2: idle, 3: sleeping, 4: sick, 5: hungry, 6: dirty, 7: bored, 8: dead
   int state;
+  int age;
 
-  // both between 0 and 1
+  // between 0 and 1
   // having these as doubles allows smaller increments than 1 per minute
+  // because logic is only updated once per minute
   double hatch_points;
   double heat_points;
+  double health;
+  double hunger;
+  double cleanliness;
+  double boredom;
 
-  // Couldn't think of a better way to store each type of state, this'll do for now
   uint32_t egg_states[2];
   uint32_t cracked_states[2];
   uint32_t idle_states[2];
@@ -48,17 +46,25 @@ struct Monster {
   uint32_t dirty_states[2];
   uint32_t bored_states[2];
   uint32_t dead_states[2];
+
+  int hatch_update;
 };
 
 static struct Monster sprat;
 
+
 // Make a default monster (sprat)
 const struct Monster MONSTER_DEFAULT = {
-  1,
+  1,  // id
   "sprat",
-  0,
-  0.0,
-  1.0, // set to 1 for testing
+  0,  // state
+  0,  // age
+  0.0,  // hatch_points
+  0.2,  // heat_points
+  1.0,  // health
+  0.0,  // hunger
+  1.0,  // cleanliness
+  0.0,  // boredom
   { RESOURCE_ID_001_SPRAT_EGG1, RESOURCE_ID_001_SPRAT_EGG2 },
   { RESOURCE_ID_001_SPRAT_CRACKED },
   { RESOURCE_ID_001_SPRAT_IDLE1, RESOURCE_ID_001_SPRAT_IDLE2 },
@@ -67,8 +73,44 @@ const struct Monster MONSTER_DEFAULT = {
   { RESOURCE_ID_001_SPRAT_HUNGRY },
   { RESOURCE_ID_001_SPRAT_DIRTY },
   { RESOURCE_ID_001_SPRAT_BORED },
-  { RESOURCE_ID_001_SPRAT_DEAD }
+  { RESOURCE_ID_001_SPRAT_DEAD },
+  INT_MAX
 };
+
+/****** Button handling ******/
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  if(sprat.state == 0) {
+    sprat.heat_points += 0.005;
+  }
+}
+
+static void click_config_provider(void *context) {
+  ButtonId id = BUTTON_ID_SELECT;
+  window_single_click_subscribe(id, select_click_handler);
+}
+
+/****** Drawing lines and stuff ******/
+static void canvas_update_proc(Layer *layer, GContext *ctx) {
+  if(sprat.state == 0) {
+    int l_edge = 10;
+    int r_edge = 134;
+    int bottom = 158;
+    int heat_point_line_end = l_edge + (r_edge - l_edge) * sprat.heat_points;
+
+    graphics_context_set_stroke_color(ctx, GColorBlack);
+    graphics_context_set_stroke_width(ctx, 5);
+    graphics_context_set_antialiased(ctx, false);
+
+    GPoint start = GPoint(l_edge, bottom);
+    GPoint end = GPoint(r_edge, bottom);
+    graphics_draw_line(ctx, start, end);
+
+    graphics_context_set_stroke_color(ctx, GColorRed);
+
+    end = GPoint(heat_point_line_end, bottom);
+    graphics_draw_line(ctx, start, end);
+  }
+}
 
 static void update_time() {
   time_t temp = time(NULL);
@@ -78,108 +120,108 @@ static void update_time() {
   text_layer_set_text(s_time_layer, s_buffer);
 }
 
+
 static void display_image(uint32_t resource) {
   gbitmap_destroy(s_bitmap);
   s_bitmap = gbitmap_create_with_resource(resource);
   bitmap_layer_set_bitmap(s_bitmap_layer, s_bitmap);
-  layer_mark_dirty(bitmap_layer_get_layer(s_bitmap_layer));
 }
 
-static void animation_loop() {
-  display_image(animation_sequence[animation_frame]);
-  animation_frame = (animation_frame + 1) % total_frames;
-  app_timer_register(1000, animation_loop, NULL);
-}
+static void display_monster_state(struct Monster m) {
+  switch(m.state){
+    case 0: // egg
+      display_image(m.egg_states[current_idle_frame]);
+      current_idle_frame = (current_idle_frame + 1) % (sizeof(m.egg_states) / sizeof(uint32_t));
+      break;
 
-static void start_idle_animation(struct Monster monster) {
-  animation_sequence[0] = monster.idle_states[0];
-  animation_sequence[1] = monster.idle_states[1];
-  total_frames = 2;
+    case 1: // hatching
+      // APP_LOG(APP_LOG_LEVEL_DEBUG, "frame: %d", hatch_animation_frame/4);
 
-  animation_loop();
-}
+      switch(hatch_animation_frame / 4){
+        case 0:
+          display_image(m.egg_states[1]);
+          break;
+        case 1:
+          display_image(m.cracked_states[0]);
+          break;
+        case 2:
+          display_image(m.idle_states[0]);
+          break;
+        default:
+          break;
+      }
+      hatch_animation_frame = hatch_animation_frame + 1;
+      break;
 
-static void single_animation() {
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Frame: %d, Total frames: %d", animation_frame, total_frames);
-  display_image(animation_sequence[animation_frame]);
-  if(animation_frame == total_frames - 1) {
-    memset(animation_sequence, 0, sizeof(animation_sequence));
-    total_frames = 0;
-    // This made the last image of this single animation not appear
-    // and is also wrong because not generic, was just for testing
-    // start_idle_animation(sprat);
-    return;
+    case 2: // idle
+      display_image(m.idle_states[current_idle_frame]);
+      current_idle_frame = (current_idle_frame + 1) % (sizeof(m.idle_states) / sizeof(uint32_t));
+      break;
   }
-  animation_frame = (animation_frame + 1) % total_frames;
-  app_timer_register(2000, single_animation, NULL);
 }
 
-static void start_hatch_animation(struct Monster monster) {
-  // uint32_t sequence[4] = { monster.egg_states[0], monster.egg_states[1], monster.cracked_states[0], monster.idle_states[0] };
-  animation_sequence[0] = monster.egg_states[0];
-  animation_sequence[1] = monster.egg_states[1];
-  animation_sequence[2] = monster.cracked_states[0];
-  animation_sequence[3] = monster.idle_states[0];
-  total_frames = 4;
-
-  single_animation();
-  return;
-}
-
-
-static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  time_t temp = mktime(tick_time);
+static void update() {
+  uint16_t ms;
+  time_ms(NULL, &ms);
+  time_t temp = time(NULL);
   int current_time = (int)temp;
 
   if(start_time == 0) {
     start_time = current_time;
-    last_time = current_time;
+    last_time = 0;
   }
 
-  int total_elapsed_time = current_time - start_time;
-  int elapsed_time = current_time - last_time;
-  last_time = current_time;
+  int total_elapsed_time_ms = (current_time - start_time) * 1000 + ms; // delta from app start in ms
+  int elapsed_time_ms = total_elapsed_time_ms - last_time; // delta from last update in ms
+  last_time = total_elapsed_time_ms;
 
-  // this is OP for testing purposes
-  // adding 0.01 per second atm (elapsed_time/100) @ full heat points
-  // 0.01 per minute = 0.00016667 per second (elapsed_time/6000)
-  if(!hatched) {
-    sprat.hatch_points += ((double)elapsed_time / 100) * sprat.heat_points;
+  if(sprat.state == 0) {
+    sprat.hatch_points += ((double)elapsed_time_ms / 100000) * sprat.heat_points;
+    sprat.heat_points -= ((double)elapsed_time_ms / 100000) / 36; // 5 every 3 minutes (i think?)
+    sprat.heat_points = sprat.heat_points >= 1 ? 1 :
+                        sprat.heat_points <= 0.01 ? 0 :
+                        sprat.heat_points;
+
+    if(sprat.hatch_points >= 1) {
+      sprat.state = 1;
+      sprat.hatch_update = updates;
+      APP_LOG(APP_LOG_LEVEL_DEBUG, "Hatching!");
+    }
   }
 
-  if(sprat.hatch_points >= 1 && !hatched) {
-    hatched = true;
-    start_hatch_animation(sprat);
-    sprat.state = 1;
-    // don't know how to get this to run AFTER the hatch animation has finished
-    // There's probably a better way to implement animating that app_timer_register
-    // but idk what it is at the moment
-    // start_idle_animation(sprat);
+  updates++;
+  if(updates % (int)(5 / (sprat.state == 0 ? sprat.heat_points : 1)) == 0) {
+    display_monster_state(sprat);
   }
 
-  // This is just for printing and testing delete before release!!
-  int big_hatch_points = (int)(sprat.hatch_points * 10000);
+  if(sprat.state == 1 && (sprat.hatch_update + 5*4*3) <= updates) {
+    sprat.state = 2;
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Hatched");
+  }
+
+  /*
+  int big_hatch_points = sprat.hatch_points * 10000;
   int hatch_whole = big_hatch_points / 100;
   int hatch_decimal = big_hatch_points % 100;
 
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Current time: %d, Total elapsed time: %d, Hatch percent: %d.%d",
-                                current_time,
-                                total_elapsed_time,
-                                hatch_whole,
-                                hatch_decimal
-  );
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Last elapsed time: %d", elapsed_time);
+  int big_heat_points = sprat.heat_points * 100000;
+  int heat_whole = big_heat_points / 1000;
+  int heat_decimal = big_heat_points % 1000;
 
-  update_time();
+
+  APP_LOG(APP_LOG_LEVEL_INFO, "s: %d, hap: %d.%02d, hep: %d.%03d, et: %d, tet: %d",
+                              sprat.state,
+                              hatch_whole,
+                              hatch_decimal,
+                              heat_whole,
+                              heat_decimal,
+                              elapsed_time_ms,
+                              total_elapsed_time_ms);
+  */
 }
 
-static void change_creature_state() {
-  current_state = (current_state + 1) % (sizeof(creature_states) / sizeof(uint32_t));
-  gbitmap_destroy(s_bitmap);
-  s_bitmap = gbitmap_create_with_resource(creature_states[current_state]);
-  bitmap_layer_set_bitmap(s_bitmap_layer, s_bitmap);
-  layer_mark_dirty(bitmap_layer_get_layer(s_bitmap_layer));
-  app_timer_register(500, change_creature_state, NULL);
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
 }
 
 static Animation * create_animation(struct GRect start, struct GRect end) {
@@ -204,8 +246,6 @@ static GRect get_random_end_rect(struct GRect start) {
 static void start_egg_sequence(struct Monster m) {
   display_image(RESOURCE_ID_001_SPRAT_EGG1);
 
-  int closeness_to_hatching = 1500;
-
   GRect start = GRect(0, 0, 144, 168);
   Animation *anim0 = create_animation(start, get_random_end_rect(start));
   Animation *anim1 = create_animation(start, get_random_end_rect(start));
@@ -217,13 +257,22 @@ static void start_egg_sequence(struct Monster m) {
 
 
   animation_schedule(final_anim);
-  // app_timer_register(closeness_to_hatching, start_egg_sequence, NULL);
+  // app_timer_register(1500, start_egg_sequence, NULL);
+}
+
+static void timer_callback() {
+  update();
+  layer_mark_dirty(bitmap_layer_get_layer(s_bitmap_layer));
+  layer_mark_dirty(s_canvas_layer);
+  s_timer = app_timer_register(delta, timer_callback, NULL);
+}
+
+static void start() {
+  display_monster_state(sprat);
+  s_timer = app_timer_register(delta, timer_callback, NULL);
 }
 
 static void main_window_load(Window *window) {
-  // lmao app logs can't be that long
-  // abcd on the end isn't printed
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "012345678901234567890123456789012345678901234567890123456789012345678901234567890123456abcd");
   sprat = MONSTER_DEFAULT;
 
   Layer *window_layer = window_get_root_layer(window);
@@ -240,19 +289,28 @@ static void main_window_load(Window *window) {
 
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 
-  s_bitmap_layer = bitmap_layer_create(bounds);
+  // Primitive drawing stuff
+  s_canvas_layer = layer_create(bounds);
 
-  //change_creature_state();
-  start_egg_sequence(sprat);
+  layer_set_update_proc(s_canvas_layer, canvas_update_proc);
+  layer_add_child(window_get_root_layer(window), s_canvas_layer);
+
+  // change_creature_state();
+  // start_egg_sequence(sprat);
+
+  // Bitmap drawing stuff
+  s_bitmap_layer = bitmap_layer_create(bounds);
 
   bitmap_layer_set_compositing_mode(s_bitmap_layer, GCompOpSet);
   layer_add_child(window_layer, bitmap_layer_get_layer(s_bitmap_layer));
+  start();
 }
 
 static void main_window_unload(Window *window) {
   bitmap_layer_destroy(s_bitmap_layer);
   gbitmap_destroy(s_bitmap);
   text_layer_destroy(s_time_layer);
+  app_timer_cancel(s_timer);
 }
 
 static void init() {
@@ -263,6 +321,8 @@ static void init() {
     .unload = main_window_unload,
   });
   window_stack_push(s_main_window, true);
+
+  window_set_click_config_provider(s_main_window, click_config_provider);
 
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   update_time();
