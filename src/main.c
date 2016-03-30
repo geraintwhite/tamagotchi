4519f3f6f2558cc6a085f85390798cd9361a0959
Geraint White <geraintwhite@gmail.com>
2016-03-30 22:01:36 +0100

    Add Creature struct

--- src/main.c
+++ src/main.c
@@ -1,47 +1,70 @@
 #include <pebble.h>
 
+#define length(array) (sizeof(array) / sizeof(array[0]))
 
-static Window *s_main_window;
-static GBitmap *s_bitmap;
-static BitmapLayer *s_bitmap_layer;
-static TextLayer *s_time_layer;
-static AppTimer *creature_state_timer;
+static Window * s_main_window;
+static GBitmap * s_bitmap;
+static BitmapLayer * s_bitmap_layer;
+static TextLayer * s_time_layer;
+static AppTimer * creature_state_timer;
 
-static uint32_t creature_states[2] = {RESOURCE_ID_001_SPRAT_IDLE1, RESOURCE_ID_001_SPRAT_IDLE2};
-static int current_state = 0;
+static uint32_t idle_states[2] = {RESOURCE_ID_001_SPRAT_IDLE1, RESOURCE_ID_001_SPRAT_IDLE2};
+
+typedef struct {
+  int age;
+  int health;
+  int hunger;
+  int cleanliness;
+  int boredom;
+  int creature_type;
+  int idle_state;
+  char name[];
+} Creature;
+
+typedef struct {
+  int points;
+  int current_creature;
+  Creature * creatures[];
+} User;
+
+static User * user;
 
 
 static void
-change_creature_state()
+change_idle_state(Creature * creature)
 {
-  current_state = (current_state + 1) % (sizeof(creature_states) / sizeof(uint32_t));
+  creature->idle_state = (creature->idle_state + 1) % length(idle_states);
   gbitmap_destroy(s_bitmap);
-  s_bitmap = gbitmap_create_with_resource(creature_states[current_state]);
+  s_bitmap = gbitmap_create_with_resource(idle_states[creature->idle_state]);
   bitmap_layer_set_bitmap(s_bitmap_layer, s_bitmap);
   layer_mark_dirty(bitmap_layer_get_layer(s_bitmap_layer));
-  creature_state_timer = app_timer_register(500, change_creature_state, NULL);
+  creature_state_timer = app_timer_register(500, (AppTimerCallback) change_idle_state, creature);
+}
+
+static void accel_tap_handler(AccelAxisType axis, int32_t direction) {
+
 }
 
 static void
 update_time()
 {
   time_t temp = time(NULL);
-  struct tm *tick_time = localtime(&temp);
+  struct tm * tick_time = localtime(&temp);
   static char s_buffer[8];
   strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
   text_layer_set_text(s_time_layer, s_buffer);
 }
 
 static void
-tick_handler(struct tm *tick_time, TimeUnits units_changed)
+tick_handler(struct tm * tick_time, TimeUnits units_changed)
 {
   update_time();
 }
 
 static void
-main_window_load(Window *window)
+main_window_load(Window * window)
 {
-  Layer *window_layer = window_get_root_layer(window);
+  Layer * window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
   s_time_layer = text_layer_create(GRect(0, 8, bounds.size.w, 50));
@@ -55,7 +78,7 @@ main_window_load(Window *window)
 }
 
 static void
-main_window_unload(Window *window)
+main_window_unload(Window * window)
 {
   bitmap_layer_destroy(s_bitmap_layer);
   gbitmap_destroy(s_bitmap);
@@ -73,8 +96,11 @@ init()
   });
   window_stack_push(s_main_window, true);
 
+  Creature creature;
+
+  accel_tap_service_subscribe(accel_tap_handler);
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-  change_creature_state();
+  change_idle_state(&creature);
   update_time();
 }
 
